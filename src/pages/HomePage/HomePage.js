730eb4d89c66ac55788855affce71016b842196b
Aviral Dewan <69675094+aviraldewan@users.noreply.github.com>
2023-11-25 18:59:15 +0530

    Update HomePage.js

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,7 +9,7 @@ import Footer from "../../components/HomePage/Footer/Footer";
 
 export default function HomePage() {
 
-    const images = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDTl17W7NL_qPVijudWlP63tpq9_OVjDTTKA&usqp=CAU", "https://img.flawlessfiles.com/_r/300x400/100/8b/df/8bdf0705ef760a65894f3506ff474576/8bdf0705ef760a65894f3506ff474576.jpg"];
+    const images = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDTl17W7NL_qPVijudWlP63tpq9_OVjDTTKA&usqp=CAU", "https://img.flawlessfiles.com/_r/300x400/100/8b/df/8bdf0705ef760a65894f3506ff474576/8bdf0705ef760a65894f3506ff474576.jpg", "https://t3.ftcdn.net/jpg/01/62/06/40/360_F_162064034_HI2YEgV7km3HMy0rccQczKH2vvpI4OnB.jpg", "https://t3.ftcdn.net/jpg/01/18/46/52/360_F_118465200_0q7Of6UnbA8kDlYEe3a4PuIyue27fbuV.jpg"];
 
   return (
     <div className={styles.container}>
